3d7b8d2e216bcae127889d3b3a454c5f0dcf2514
MoriP <kohei.moriyama426@gmail.com>
2025-07-05 17:34:29 +0900

    fixed

--- cpp_00/ex02/Account.cpp
+++ cpp_00/ex02/Account.cpp
@@ -1,14 +1,62 @@
 #include "Account.hpp"
 
-Account::Account() {};
+#include <iostream>
+
+Account::Account(): _accountIndex(0), _amount(0), _nbDeposits(0), _nbWithdrawals(0) {};
 Account::~Account() {};
 
 void	Account::displayAccountsInfos(void)
 {
-	
+	std::cout << "infos" << std::endl;
 }
 
-int main(int argc, char const *argv[])
+void	Account::displayStatus() const
 {
-	return 0;
+	std::cout << "status" << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	std::cout << "deposit" << std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	std::cout << "withdrawal" << std::endl;
+	return true;
+}
+
+int	Account::checkAmount(void) const
+{
+	return 5;
+}
+
+int	Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
+}
+
+void	Account::_displayTimestamp(void)
+{
+	std::cout << "timestamp" << std::endl;
+}
+
+Account::Account(void)
+{
+	std::cout << "Account" << std::endl;
 }
