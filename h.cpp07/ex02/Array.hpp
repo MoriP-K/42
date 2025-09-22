0a455f9634bd33fd2f2a8b4c94ffef1eec083637
MoriP <kohei.moriyama426@gmail.com>
2025-09-22 20:44:46 +0900

    FIN: ex02 9/22

--- h.cpp07/ex02/Array.hpp
+++ h.cpp07/ex02/Array.hpp
@@ -1,13 +1,36 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+#include <exception>
+#include <cstdlib>
+
+template <typename T>
 class Array
 {
 private:
-	
+	T	*_arr;
+	unsigned int	_size;
+
 public:
 	Array();
+	Array(unsigned int n);
+	Array(Array const &copy);
 	~Array();
+
+	Array &operator=(Array const &src);
+	T &operator[](unsigned int i);
+	T const &operator[](unsigned int i) const;
+
+	unsigned int size() const;
+
+	class OutOfBoundsException: public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
 };
+
+#include "Array.tpp"
 
 #endif
