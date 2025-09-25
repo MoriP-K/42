1b6c34a42bcb07693cf49066e3a451b7a1f8d8ee
MoriP <kohei.moriyama426@gmail.com>
2025-09-25 21:33:40 +0900

    WIP: ex02 9/25

---
+++ i.cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,25 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+
+class MutantStack
+{
+private:
+	
+public:
+	MutantStack();
+	~MutantStack();
+};
+
+MutantStack::MutantStack()
+{
+}
+
+MutantStack::~MutantStack()
+{
+}
+
+#include "MutantStack.tpp"
+
+#endif
