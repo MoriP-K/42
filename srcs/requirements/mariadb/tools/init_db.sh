bbe1bea9fa4dfbb58d11471105b986c9066aca0a
kmoriyam <kmoriyam@debian-inception.myguest.virtualbox.org>
2025-12-22 22:49:13 +0900

    OK: mariadb container

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,21 +1,42 @@
-#! /bin/bash
+#!/bin/bash
+
+set -e
 
 echo "Initialize MariaDB..."
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing DB..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    echo "Setting up database and users..."
     mysqld --user=mysql --bootstrap << EOF
-UPDATE mysql.user SET Password=PASSWORD('${DB_ROOT_PASS}') WHERE User='root';
+USE mysql;
+FLUSH PRIVILEGES;
+
+-- rootパスワード設定
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
+
+-- 不要なユーザー削除
 DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+-- testデータベース削除
 DROP DATABASE IF EXISTS test;
-CREATE DATABASE ${DB_NAME};
-CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+
+-- WordPressデータベース作成
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+
+-- WordPressユーザー作成
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+
+-- 権限反映
 FLUSH PRIVILEGES;
 EOF
 
-        echo "Complete initializing"
+    echo "Complete initializing"
+else
+    echo "Database already initialized, skipping setup"    
 fi
 
 echo "Launching MariaDB..."
