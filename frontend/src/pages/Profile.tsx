36e1c1543eb765e6e5750efede87170d592831f0
yohanata42 <yoshikohatanaka1998@gmail.com>
2026-01-23 16:42:45 +0900

    i can api communication happy

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,8 +1,44 @@
 import Footer from "../components/Footer"
+import React, { useEffect, useState } from "react";
+import { apiClient } from '../api/apiClient';
 
 function  Profile() {
-	return (
+    
+    // 1ユーザのデータがそのまま帰ってくる
+    const [profileData, setProfileData] = useState<any>(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const currentUserId = 1;
+                const data = await apiClient(`/profile?userId=${currentUserId}`);
+                setProfileData(data);
+            } catch (error) {
+                console.error("データの取得に失敗しました", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchProfile();
+    }, []);
+
+    if (isLoading) {
+        return <div className="p-10">読み込み中...</div>;
+    }
+    if (!profileData) {
+        return <div className="p-10">データを表示できません</div>;
+    }
+	
+    return (
     <div>
+        <h1 className="text-5xl font-bold">プロフィール</h1>
+            <p>名前: {profileData.name}</p>
+                <p className="font-bold">実績</p>
+                    <p>トータルスコア: {profileData.total_score}</p>
+                    <p>1位回数: {profileData.first_place_count}</p>
+                    <p>プレイ回数: {profileData.play_count}</p>
         <Footer></Footer>
     </div>
 	)
