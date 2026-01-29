4b27f210cbace84d2039484e66f37656a18bab4c
funa <orz.home35@gmail.com>
2026-01-30 05:19:56 +0900

    📝 フロントは戻した

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import Prepare from './pages/Prepare'
 import Game from './pages/Game'
 import TermsOfService from './pages/TermsOfService'
 import AccountRegister from './pages/AccountRegister'
-import Login from './pages/Login'
 import PaaswordResetSendMail from './pages/PaaswordResetSendMail'
 import PaaswordReset from './pages/PasswordReset'
 import PrivacyPolicy from './pages/PrivacyPolicy'
@@ -19,10 +18,9 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/waiting-game/:id" element={<WaitingGame />} />
 				<Route path="/prepare" element={<Prepare />} />
-				<Route path="/game" element={<Game />} />
+				<Route path="/game" element={<Game />}/>
 				<Route path="/terms" element={<TermsOfService />} />
 				<Route path="/register" element={<AccountRegister />} />
-				<Route path="/login" element={<Login />} />
 				<Route path="/password-reset/send-mail" element={<PaaswordResetSendMail />} />
 				<Route path="/password-reset" element={<PaaswordReset />} />
 				<Route path="/login/redirect" element={<RedirectLogin />} />
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,34 +1,10 @@
-import { useEffect } from 'react';
-import { userApi } from '../api/userApi'
-import { ApiError } from '../api/apiClient'
-
-
 import Footer from "../components/Footer"
 
-const Login = () => {
-	useEffect(() => {
-		const run = async () => {
-			try {
-				const me = await userApi.getMe()
-				console.log('me:', me)
-			} catch (e) {
-				if (e instanceof ApiError) {
-					console.error('API Error', {
-						status: e.status,
-						data: e.data, // 例: { message: "Unauthorized" }
-					})
-				} else {
-					console.error('Unknown Error', e)
-				}
-			}
-		}
-
-		run()
-	}, []);
+function  Login() {
 	return (
-		<div>
-			<Footer></Footer>
-		</div>
+    <div>
+        <Footer></Footer>
+    </div>
 	)
 }
 
