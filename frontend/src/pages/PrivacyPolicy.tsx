d9a181112abf5b19bfdb5a7068f402b0ed49e2e2
Yoshiko Hatanaka <yohatana@c6r3s5.42tokyo.jp>
2026-02-01 15:09:35 +0900

    自動整形噛ました

--- frontend/src/pages/PrivacyPolicy.tsx
+++ frontend/src/pages/PrivacyPolicy.tsx
@@ -1,24 +1,24 @@
-import Footer from "../components/footer/Footer"
-import PrivacyPolicyContent from "../components/footer/PrivacyPolicyContent"
-import BackButton from "../components/BackButton"
-
+import Footer from "../components/footer/Footer";
+import PrivacyPolicyContent from "../components/footer/PrivacyPolicyContent";
+import BackButton from "../components/BackButton";
 
 function PrivacyPolicy() {
 	return (
 		<div>
 			<div className="hero min-h-[80vh]">
-			<div className="hero-content text-center">
-			<div className="max-w-md">
-				<h1 className="text-5xl font-bold">プライバシーポリシー</h1>
-					<PrivacyPolicyContent>
-					</PrivacyPolicyContent>
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<h1 className="text-5xl font-bold">
+							プライバシーポリシー
+						</h1>
+						<PrivacyPolicyContent></PrivacyPolicyContent>
+					</div>
+				</div>
 			</div>
-		</div>
-		</div>
 			<BackButton></BackButton>
 			<Footer></Footer>
 		</div>
-	)
+	);
 }
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
