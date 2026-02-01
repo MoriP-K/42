968300cac847455fe3781cbd3074f3398cb41e23
Yoshiko Hatanaka <yohatana@c6r3s5.42tokyo.jp>
2026-02-01 15:11:15 +0900

    auto indent

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,9 +1,8 @@
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
-import { apiClient } from '../api/apiClient';
+import { apiClient } from "../api/apiClient";
 
 const Profile = () => {
-
 	// 1ユーザのデータがそのまま帰ってくる
 	const [profileData, setProfileData] = useState<any>(null);
 	const [isLoading, setIsLoading] = useState(true);
@@ -12,7 +11,9 @@ const Profile = () => {
 		const fetchProfile = async () => {
 			try {
 				const currentUserId = 1;
-				const data = await apiClient(`/profile?userId=${currentUserId}`);
+				const data = await apiClient(
+					`/profile?userId=${currentUserId}`
+				);
 				setProfileData(data);
 			} catch (error) {
 				console.error("データの取得に失敗しました", error);
@@ -34,18 +35,18 @@ const Profile = () => {
 	return (
 		<div>
 			<h1 className="text-5xl font-bold">プロフィール</h1>
-				<p>ユーザ名 {profileData.name}</p>
-					<p className="font-bold">実績</p>
-						<p>トータルスコア: {profileData.total_score}</p>
-						<p>1位回数: {profileData.first_place_count}</p>
-						<p>プレイ回数: {profileData.play_count}</p>
+			<p>ユーザ名 {profileData.name}</p>
+			<p className="font-bold">実績</p>
+			<p>トータルスコア: {profileData.total_score}</p>
+			<p>1位回数: {profileData.first_place_count}</p>
+			<p>プレイ回数: {profileData.play_count}</p>
 
 			<div>
 				<p className="font-bold">バッジ</p>
 			</div>
 			<Footer></Footer>
 		</div>
-	)
-}
+	);
+};
 
-export default Profile
+export default Profile;
