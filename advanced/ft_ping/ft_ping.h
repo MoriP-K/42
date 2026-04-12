a0b54b76095879aa691d88b36615c1228ab77354
MoriP-K <kohei.moriyama426@gmail.com>
2026-04-12 17:08:02 +0900

    add: same print

--- advanced/ft_ping/ft_ping.h
+++ advanced/ft_ping/ft_ping.h
@@ -6,7 +6,7 @@
 /*   By: morip <morip@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 16:19:34 by morip             #+#    #+#             */
-/*   Updated: 2026/04/11 21:52:19 by morip            ###   ########.fr       */
+/*   Updated: 2026/04/12 15:24:58 by morip            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,27 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <math.h>
 
 volatile sig_atomic_t intr = 0; // for handling signal
 
 typedef struct s_icmp
 {
-	u_int8_t type;
-	u_int8_t code;
-	u_int16_t checksum;
-	u_int16_t id;
-	u_int16_t sequence;
-	uint32_t data;
-} t_icmp __attribute__((packed));
+	uint8_t type;
+	uint8_t code;
+	uint16_t checksum;
+	uint16_t id;
+	uint16_t sequence;
+	// uint32_t data;
+	char data[56];
+} __attribute__((packed)) t_icmp;
+
+typedef struct s_res
+{
+	char buf[1024];
+	struct sockaddr_in from;
+	socklen_t from_len;
+} t_res;
 
 typedef struct s_stat
 {
@@ -42,9 +51,26 @@ typedef struct s_stat
 	struct timeval end;
 	double max;
 	double min;
+	double avg;
+	double m2;
+	double stddev;
 	double total;
+	unsigned char ttl;
 	double rtt;
 	double time;
+	size_t sequence;
+	size_t received;
 	size_t count;
 	char *hostname;
 } t_stat;
+
+typedef struct s_ping
+{
+	t_icmp packet;
+	t_stat stat;
+	t_res res;
+	struct addrinfo *ai;
+	struct addrinfo hints;
+	struct sockaddr_in sock_in;
+	int sock_fd;
+} t_ping;
--- advanced/ft_ping/main.c
+++ advanced/ft_ping/main.c
@@ -6,7 +6,7 @@
 /*   By: morip <morip@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 16:18:38 by morip             #+#    #+#             */
-/*   Updated: 2026/04/11 21:52:38 by morip            ###   ########.fr       */
+/*   Updated: 2026/04/12 17:07:05 by morip            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ u_int16_t checksum(void *data, size_t len)
 	u_int32_t sum;
 	u_int16_t *ptr;
 
-	// printf("len: %zu\n", len);
 	sum = 0;
 	ptr = (u_int16_t *)data;
 	while (len > 1)
@@ -44,128 +43,138 @@ void arg_error(void)
 
 void signal_handler(int signum)
 {
+	(void)signum;
 	intr = 1;
 }
 
 void print_result(t_stat stat, struct addrinfo *ai)
 {
+	double loss;
+
+	loss = 0;
+	if (stat.sequence > 0)
+		loss = (1.0 - (double)stat.received / stat.sequence) * 100.0;
 	printf("--- %s ft_ping statistics ---\n", stat.hostname);
-	printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/test ms\n", stat.min, stat.total / stat.count, stat.max);
+	printf("%lu packets transmitted, %lu packets received, %.0f%% packet loss\n", stat.sequence, stat.received, loss);
+	printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms\n", stat.min, stat.total / stat.count, stat.max, stat.stddev);
 	freeaddrinfo(ai);
 	exit(1);
 }
 
-// initialize vars func
+void init_memory(t_ping *ping)
+{
+	memset(ping, 0, sizeof(t_ping));
+	memset(&ping->ai, 0 , sizeof(struct addrinfo *));
+	memset(&ping->hints, 0 , sizeof(struct addrinfo));
+	memset(&ping->stat, 0, sizeof(t_stat));
+	memset(&ping->packet, 0, sizeof(t_icmp));
+	memset(&ping->sock_in, 0, sizeof(struct sockaddr_in));
+	memset(&ping->res, 0, sizeof(t_res));
+}
+
+void update_stat(t_stat *stat, double latest_time, size_t count, char *buf)
+{
+	double old_avg;
+	double variance;
+
+	old_avg = stat->avg;
+	stat->avg += (latest_time - old_avg) / count;
+	stat->m2 += (latest_time - old_avg) * (latest_time - stat->avg);
+	variance = stat->m2 / count;
+	stat->stddev = sqrt(variance);
+	stat->ttl = (unsigned char)buf[8];
+	if (stat->min == 0 && stat->max == 0)
+	{
+		stat->min = latest_time;
+		stat->max = latest_time;
+	}
+	if (latest_time < stat->min)
+		stat->min = latest_time;
+	if (stat->max < latest_time)
+		stat->max = latest_time;
+	stat->total += latest_time;
+}
 
 int main(int ac, char *av[])
 {
+	t_ping ping;
+	ssize_t n;
+	ssize_t rec;
+	t_icmp *reply;
+
 	if (ac < 1)
 		arg_error();
 	// add: option from args
 
-	// add: getaddrinfo()
-	struct addrinfo *ai;
-	struct addrinfo hints;
+	init_memory(&ping);
 
-	memset(&ai, 0 , sizeof(struct addrinfo *));
-	memset(&hints, 0 , sizeof(struct addrinfo));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_RAW;
-	hints.ai_protocol = IPPROTO_ICMP;
+	ping.hints.ai_family = AF_INET;
+	ping.hints.ai_socktype = SOCK_RAW;
+	ping.hints.ai_protocol = IPPROTO_ICMP;
 	// hints.ai_flags = AI_CANONNAME; // ?
 
-	t_stat stat;
-
-	memset(&stat, 0, sizeof(t_stat));
-	stat.hostname = av[1];
-	if (getaddrinfo(stat.hostname, NULL, (const struct addrinfo *)&hints, &ai) != 0)
+	ping.stat.hostname = av[1];
+	if (getaddrinfo(ping.stat.hostname, NULL, (const struct addrinfo *)&ping.hints, &ping.ai) != 0)
 	{
 		perror("getaddrinfo");
 		return (1);
 	}
 
-	int sock_fd = socket(AF_INET, SOCK_RAW, 1);
-	if (sock_fd == -1)
+	ping.sock_fd = socket(AF_INET, SOCK_RAW, 1);
+	if (ping.sock_fd == -1)
 	{
 		perror("socket");
 	}
 
-	t_icmp packet;
+	ping.packet.type = 8; // Echo Request
+	ping.packet.code = 0;
+	ping.packet.id = 0;
 
-	memset(&packet, 0, sizeof(t_icmp));
-	packet.type = 8; // Echo Request
-	packet.code = 0;
-	packet.id = 0;
-	packet.sequence = 0;
-	packet.data = 0xdeadbeef;
-	packet.checksum = checksum(&packet, sizeof(packet));
-	// printf("checksum: %x\n", packet.checksum);
+	ping.sock_in.sin_family = AF_INET;
+	ping.sock_in.sin_port = 0;
 
-	unsigned char *raw = (unsigned char *)&packet;
-	// printf("Memory dump (first 4 bytes): %02x %02x %02x %02x\n", raw[0], raw[1], raw[2], raw[3]);
+	struct sockaddr_in *sin = (struct sockaddr_in *)ping.ai->ai_addr;
+	char *ip = inet_ntoa(sin->sin_addr);
+	printf("FT_PING %s (%s): %lu data bytes\n", ping.stat.hostname, ip, sizeof(t_icmp));
+	ping.sock_in.sin_addr = sin->sin_addr;
 
-	struct sockaddr_in sock_in;
-
-	memset(&sock_in, 0, sizeof(struct sockaddr_in));
-	sock_in.sin_family = AF_INET;
-	sock_in.sin_port = 0;
-
-	printf("FT_PING %s (%%s): %%lu data bytes\n", stat.hostname);
-
-	// add signal handle
 	signal(SIGINT, signal_handler);
+	struct timeval tv;
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+	setsockopt(ping.sock_fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
 	while (1)
 	{
-		gettimeofday(&stat.start, NULL);
-		ssize_t n = sendto(sock_fd, &packet, sizeof(packet), 0, (struct sockaddr *)&sock_in, sizeof(sock_in));
+		ping.packet.sequence = htons(ping.stat.sequence++);
+		ping.packet.checksum = 0;
+		ping.packet.checksum = checksum(&ping.packet, sizeof(ping.packet));
+		gettimeofday(&ping.stat.start, NULL);
+		n = sendto(ping.sock_fd, &ping.packet, sizeof(ping.packet), 0, (struct sockaddr *)&ping.sock_in, sizeof(ping.sock_in));
 		if (n < 0)
 		{
 			perror("sendto");
-			printf("errno: %d\n", errno);
 			break;
 		}
-		char buf[1024];
-		struct sockaddr_in from;
-		socklen_t from_len;
-		from_len = sizeof(from);
-		ssize_t rec = recvfrom(sock_fd, buf, sizeof(buf), 0, (struct sockaddr *)&from, &from_len);
+		ping.res.from_len = sizeof(ping.res.from);
+		rec = recvfrom(ping.sock_fd, ping.res.buf, sizeof(ping.res.buf), 0, (struct sockaddr *)&ping.res.from, &ping.res.from_len);
+		if (intr == 1 || (rec < 0 && errno == EINTR))\
+			print_result(ping.stat, ping.ai);
 		if (rec > 0)
 		{
-			struct t_icmp *reply = (struct t_icmp *)(buf + 20);
-			// add: print result
-			if (intr == 1)
-				print_result(stat, ai);
-			// add: Round Trip Time
-			stat.rtt = 0;
-			gettimeofday(&stat.end, NULL);
-			stat.rtt = (double)(stat.end.tv_sec - stat.start.tv_sec) * 1000.0;
-			stat.rtt += (double)(stat.end.tv_usec - stat.start.tv_usec) / 1000.0;
-			stat.time = stat.rtt;
-			if (stat.min == 0 && stat.max == 0)
-			{
-				stat.min = stat.rtt;
-				stat.max = stat.rtt;
-			}
-			if (stat.rtt < stat.min)
-				stat.min = stat.rtt;
-			if (stat.max < stat.rtt)
-				stat.max = stat.rtt;
-			stat.total += stat.rtt;
-			stat.count++;
-			printf("%lu bytes from %s: icmp_seq=%u ttl=%%u time=%.3f ms\n", sizeof(packet), stat.hostname, stat.count, 123, stat.time);
-			// printf("received type: %d, code: %d\n", reply->type, reply->code);
+			if (errno == EINTR)
+				print_result(ping.stat, ping.ai);
+			reply = (t_icmp *)(ping.res.buf + 20);
+			if (reply->type != 0)
+				continue;
+			gettimeofday(&ping.stat.end, NULL);
+			ping.stat.time = 0;
+			ping.stat.time = (double)(ping.stat.end.tv_sec - ping.stat.start.tv_sec) * 1000.0;
+			ping.stat.time += (double)(ping.stat.end.tv_usec - ping.stat.start.tv_usec) / 1000.0;
+			update_stat(&ping.stat, ping.stat.time, ++ping.stat.count, ping.res.buf);
+			printf("%lu bytes from %s: icmp_seq=%lu ttl=%u time=%.3f ms\n", sizeof(ping.packet), ping.stat.hostname, ping.stat.received++, ping.stat.ttl, ping.stat.time);
 		}
+		if (intr == 1)
+			print_result(ping.stat, ping.ai);
 		sleep(1);
 	}
 }
-
-/* LINUX print ping
-root@3df9d4d01fd5:/app# ping google.com
-PING google.com (172.217.211.100): 56 data bytes
-64 bytes from 172.217.211.100: icmp_seq=0 ttl=110 time=20.887 ms
-64 bytes from 172.217.211.100: icmp_seq=1 ttl=110 time=12.624 ms
-64 bytes from 172.217.211.100: icmp_seq=2 ttl=110 time=12.370 ms
-^C--- google.com ping statistics ---
-3 packets transmitted, 3 packets received, 0% packet loss
-round-trip min/avg/max/stddev = 12.370/15.294/20.887/3.956 ms
-*/
